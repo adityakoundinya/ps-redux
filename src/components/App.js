da6ab125c1c063e366eb18b3296b296b9937da4c
Aditya Koundinya <aditya.koundinya@gmail.com>
2020-01-21 16:06:38 -0600

    Adding routing and courses page

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
+import Header from "./common/Header";
+import PageNotFound from "./PageNotFound";
+import CoursesPage from "./courses/CoursesPage";
 
 function App() {
   return (
     <div className="container-fluid">
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/about" component={AboutPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/courses" component={CoursesPage} />
+        <Route component={PageNotFound}></Route>
+      </Switch>
     </div>
   );
 }
