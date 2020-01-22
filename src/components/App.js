8bae08a20fd194dc4e3ab0bec08ab74d69be24f9
Aditya Koundinya <aditya.koundinya@gmail.com>
2020-01-22 16:54:33 -0600

    Adding course add, update page and actions.

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
 import PageNotFound from "./PageNotFound";
 import CoursesPage from "./courses/CoursesPage";
+import ManageCoursePage from "./courses/ManageCoursePage";
 
 function App() {
   return (
@@ -12,8 +13,10 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/about" component={AboutPage} />
-        <Route exact path="/courses" component={CoursesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/course" component={ManageCoursePage} />
         <Route component={PageNotFound}></Route>
       </Switch>
     </div>
--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,17 +5,22 @@ import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
+import { Redirect } from "react-router-dom";
 
 class CoursesPage extends React.Component {
+  state = {
+    redirectToAddCoursePage: false
+  };
+
   componentDidMount() {
     const { courses, authors, actions } = this.props;
 
-    if (courses.length == 0) {
+    if (courses.length === 0) {
       actions.loadCourses().catch(error => {
         alert("Loading courses failed" + error);
       });
     }
-    if (authors.length == 0) {
+    if (authors.length === 0) {
       actions.loadAuthors().catch(error => {
         alert("Loading authors failed" + error);
       });
@@ -24,7 +29,15 @@ class CoursesPage extends React.Component {
   render() {
     return (
       <>
+        {this.state.redirectToAddCoursePage && <Redirect to="/course" />}
         <h2>Courses</h2>
+        <button
+          style={{ marginBottom: 20 }}
+          className="btn btn-primary add-course"
+          onClick={() => this.setState({ redirectToAddCoursePage: true })}
+        >
+          Add Course
+        </button>
         <CourseList courses={this.props.courses} />
       </>
     );
