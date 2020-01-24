e033df8d33a96ca04944ed2c4e2449255dc49695
Adi Koundinya <aditya.koundinya@gmail.com>
2020-01-23 23:43:33 -0600

    Ordering CourseList by title.

--- src/components/courses/CourseList.js
+++ src/components/courses/CourseList.js
@@ -14,33 +14,41 @@ const CourseList = ({ courses, onDeleteClick }) => (
       </tr>
     </thead>
     <tbody>
-      {courses.map(course => {
-        return (
-          <tr key={course.id}>
-            <td>
-              <a
-                className="btn btn-light"
-                href={"http://pluralsight.com/courses/" + course.slug}
-              >
-                Watch
-              </a>
-            </td>
-            <td>
-              <Link to={"/course/" + course.slug}>{course.title}</Link>
-            </td>
-            <td>{course.authorName}</td>
-            <td>{course.category}</td>
-            <td>
-              <button
-                className="btn btn-outline-danger"
-                onClick={() => onDeleteClick(course)}
-              >
-                Delete
-              </button>
-            </td>
-          </tr>
-        );
-      })}
+      {courses
+        .sort((a, b) => {
+          var tA = a.title.toUpperCase();
+          var tB = b.title.toUpperCase();
+          if (tA < tB) return -1;
+          if (tA > tB) return 1;
+          return 0;
+        })
+        .map(course => {
+          return (
+            <tr key={course.id}>
+              <td>
+                <a
+                  className="btn btn-light"
+                  href={"http://pluralsight.com/courses/" + course.slug}
+                >
+                  Watch
+                </a>
+              </td>
+              <td>
+                <Link to={"/course/" + course.slug}>{course.title}</Link>
+              </td>
+              <td>{course.authorName}</td>
+              <td>{course.category}</td>
+              <td>
+                <button
+                  className="btn btn-outline-danger"
+                  onClick={() => onDeleteClick(course)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          );
+        })}
     </tbody>
   </table>
 );
